0c714239df46a2058cb2c664df7c391271f2b73c
Andreas Kondylis <akondylis@gmail.com>
2014-12-22 10:10:45 +0200

    - fixes

--- frontend/views/ui-krf/site/about.php
+++ frontend/views/ui-krf/site/about.php
@@ -4,10 +4,193 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'About';
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="well well-sm well-title">
+    <strong>About us</strong>
+</div>
+<div class="well well-sm">
+    <div class="panel-group" id="accordion">
+        <h1>h1. Bootstrap heading</h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+        <h2>h2. Bootstrap heading</h2>
 
-    <code><?= __FILE__ ?></code>
+        <h3>h3. Bootstrap heading</h3>
+        <h4>h4. Bootstrap heading</h4>
+        <h5>h5. Bootstrap heading</h5>
+        <h6>h6. Bootstrap heading</h6>
+
+        <h1>h1. Bootstrap heading
+            <small>Secondary text</small>
+        </h1>
+        <h2>h2. Bootstrap heading
+            <small>Secondary text</small>
+        </h2>
+        <h3>h3. Bootstrap heading
+            <small>Secondary text</small>
+        </h3>
+        <h4>h4. Bootstrap heading
+            <small>Secondary text</small>
+        </h4>
+        <h5>h5. Bootstrap heading
+            <small>Secondary text</small>
+        </h5>
+        <h6>h6. Bootstrap heading
+            <small>Secondary text</small>
+        </h6>
+        <p class="lead">
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+            auctor. Duis mollis, est non commodo luctus.
+        </p>
+
+        <p class="text-lowercase">
+            Lowercased text.
+        </p>
+
+        <p class="text-uppercase">
+            Uppercased text.
+        </p>
+
+        <p class="text-capitalize">
+            Capitalized text.
+        </p>
+
+        <address>
+            <strong>Twitter, Inc.</strong>
+            <br>
+            795 Folsom Ave, Suite 600
+            <br>
+            San Francisco, CA 94107
+            <br>
+            <abbr title="Phone">P:</abbr> (123) 456-7890
+        </address>
+
+        <address>
+            <strong>Full Name</strong>
+            <br>
+            <a href="mailto:#">first.last@example.com</a>
+        </address>
+
+        <blockquote>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante.
+            </p>
+        </blockquote>
+        <ul>
+            <li>
+                Lorem ipsum dolor sit amet
+            </li>
+            <li>
+                Consectetur adipiscing elit
+            </li>
+            <li>
+                Integer molestie lorem at massa
+            </li>
+            <li>
+                Facilisis in pretium nisl aliquet
+            </li>
+            <li>
+                Nulla volutpat aliquam velit
+                <ul>
+                    <li>
+                        Phasellus iaculis neque
+                    </li>
+                    <li>
+                        Purus sodales ultricies
+                    </li>
+                    <li>
+                        Vestibulum laoreet porttitor sem
+                    </li>
+                    <li>
+                        Ac tristique libero volutpat at
+                    </li>
+                </ul>
+            </li>
+            <li>
+                Faucibus porta lacus fringilla vel
+            </li>
+            <li>
+                Aenean sit amet erat nunc
+            </li>
+            <li>
+                Eget porttitor lorem
+            </li>
+        </ul>
+        <dl class="dl-horizontal">
+            <dt>
+                Description lists
+            </dt>
+            <dd>
+                A description list is perfect for defining terms.
+            </dd>
+            <dt>
+                Euismod
+            </dt>
+            <dd>
+                Vestibulum id ligula porta felis euismod semper eget lacinia
+                odio sem nec elit.
+            </dd>
+            <dd>
+                Donec id elit non mi porta gravida at eget metus.
+            </dd>
+            <dt>
+                Malesuada porta
+            </dt>
+            <dd>
+                Etiam porta sem malesuada magna mollis euismod.
+            </dd>
+            <dt>
+                Felis euismod semper eget lacinia
+            </dt>
+            <dd>
+                Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus.
+            </dd>
+        </dl>
+        <ol>
+            <li>
+                Lorem ipsum dolor sit amet
+            </li>
+            <li>
+                Consectetur adipiscing elit
+            </li>
+            <li>
+                Integer molestie lorem at massa
+            </li>
+            <li>
+                Facilisis in pretium nisl aliquet
+            </li>
+            <li>
+                Nulla volutpat aliquam velit
+            </li>
+            <li>
+                Faucibus porta lacus fringilla vel
+            </li>
+            <li>
+                Aenean sit amet erat nunc
+            </li>
+            <li>
+                Eget porttitor lorem
+            </li>
+        </ol>
+
+        <p class="bg-class bg-primary">
+            Nullam id dolor id nibh ultricies vehicula ut id elit.
+        </p>
+
+        <p class="bg-class bg-success">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </p>
+
+        <p class="bg-class bg-info">
+            Maecenas sed diam eget risus varius blandit sit amet non magna.
+        </p>
+
+        <p class="bg-class bg-warning">
+            Etiam porta sem malesuada magna mollis euismod.
+        </p>
+
+        <p class="bg-class bg-danger">
+            Donec ullamcorper nulla non metus auctor fringilla.
+        </p>
+    </div>
 </div>
